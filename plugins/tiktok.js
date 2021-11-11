254581520ffa4f6e2c1c02fa9db84ea5dadd340c
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-11-11 19:50:02 +0700

    update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,7 +3,7 @@ cmd.get("tiktok", async(m, args) => {
 	const fetch = require('node-fetch')
 	try { 
 	var tiktok = await api.tiktok(args[0])
-	cmd.send3ButtonImg(m.chat,  await (await fetch(tiktok.thumb)).buffer(), `${tiktok.caption}`, 'By Tiktok Downloader', 'Without Watermark', `>nowm ${tiktok.nowm}`, 'With Watermark', `>wm ${tiktok.wm}`, 'Only Audio', `>ttaudio ${tiktok.music}`, m)
+	cmd.send2ButtonVideo(m.chat,  await (await fetch(tiktok.nowm)).buffer(), `${tiktok.caption}`, 'By Tiktok Downloader', 'With Watermark', `>wm ${tiktok.wm}`, 'Only Audio', `>ttaudio ${tiktok.music}`, m)
   	} catch(err) {
   		console.log(err)
   		m.reply('Terjadi kesalahan dalam mengambil media! Mohon tunggu beberapa saat lagi')
